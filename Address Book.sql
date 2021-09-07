a5a6fb6c43eaaf87d104366b053035415b4a3aed
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-09-07 11:23:56 -0600

    Feat: Created address book table

--- Address Book.sql
+++ Address Book.sql
@@ -4,3 +4,15 @@
 CREATE DATABASE AddressBookService
 select * from sys.databases
 use AddreeBookService
+--UC2
+create table AddressBookTable
+	( Id int identity(1, 1) primary Key,
+	FirstName varchar(50),
+	LastName varchar(50),
+	Address varchar(150),
+	City varchar(25),
+	State varchar(25),
+	Zipcode varchar(6),
+	PhoneNumber varchar(10),
+	email varchar(30)	
+	)
